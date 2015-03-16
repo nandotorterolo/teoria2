aece60ffba8817ce72e00c6d69b900cfd3ed3de2
Fernando Torterolo <fernando.torterolo@autodesk.com>
2015-03-16 16:22:21 -0300

    using assertThar over assertTrue

--- src/test/java/org/ucu/teoria2/actividad1/TestValidos.java
+++ src/test/java/org/ucu/teoria2/actividad1/TestValidos.java
@@ -2,16 +2,17 @@ package org.ucu.teoria2.actividad1;
 
 import junit.framework.TestCase;
 import org.ucu.utils.ReadStringFromFileLineByLine;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
 /**
  * Creado el dia 15/03/15,
  * Integrantes: Fernando Torterolo.
+ *  http://www.objectpartners.com/2013/09/18/the-benefits-of-using-assertthat-over-other-assert-methods-in-unit-tests/
  */
 public class TestValidos extends TestCase {
 
-
   String [] validos;
-  boolean res;
 
   protected void setUp(){
     validos = ReadStringFromFileLineByLine.read("CadenasValidas.txt");
@@ -20,9 +21,7 @@ public class TestValidos extends TestCase {
   public void testCases() {
     for (String str : validos)
     {
-      res = ParserNumeros.parsear(str);
-      System.out.println( "El resultado de parsear la entrada valida: '" + str + "' es => " + res);
-      assertTrue(res);
+      assertThat("El resultado de parsear la entrada valida: '" + str, ParserNumeros.parsear(str) , is(true));
     }
   }
 
